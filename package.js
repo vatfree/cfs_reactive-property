cc374bedae22d6beb5c6b034d8c1edcd83bf4ef4
Morten Henriksen <mh@gi2.dk>
2014-12-17 14:19:45 +0100

    mbr fixed warnings

--- package.js
+++ package.js
@@ -1,19 +1,20 @@
 Package.describe({
   name: 'cfs:reactive-property',
-  version: '0.0.0',
-  summary: "Reactive Property is a small, fast reative property class"
+  version: '0.0.3',
+  summary: 'Reactive Property is a small, fast reative property class',
+  git: 'https://github.com/CollectionFS/Meteor-reactive-property.git'
 });
 
-Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1');
+Package.onUse(function (api) {
+  api.versionsFrom('1.0');
   
   api.use('deps', ['client', 'server']);
 
-  api.export && api.export('ReactiveProperty');
-  api.add_files(['reactive-property.js'], ['client', 'server']);
+  api.export('ReactiveProperty');
+  api.addFiles(['reactive-property.js'], ['client', 'server']);
 });
 
-// Package.on_test(function (api) {
+// Package.onTest(function (api) {
 //   api.use('power-queue');
 //   api.use('test-helpers', 'server');
 //   api.use('tinytest');
